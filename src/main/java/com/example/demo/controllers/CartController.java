1ff024f73c14e11d2b5c872a577e33539109f134
Karthik3631 <karthikbnaik2003@gmail.com>
2025-09-16 22:54:55 +0530

    update 16 sept

--- src/main/java/com/example/demo/controllers/CartController.java
+++ src/main/java/com/example/demo/controllers/CartController.java
@@ -6,23 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
 import com.example.demo.repositories.UserRepository;
 import com.example.demo.service.CartService;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("api/cart")
 @CrossOrigin(origins="http://localhost:5173",allowCredentials = "true")
 public class CartController {
-	
+
 	@Autowired
 	CartService cartService;
-	
+
 	@Autowired
 	UserRepository userRepository;
 
@@ -30,13 +36,47 @@ public class CartController {
 	public ResponseEntity<Void> addToCart(@RequestBody Map<String, Object> request) {
 		String username = (String) request.get("username");
 		int productId = (int) request.get("productId");
-		
+
 		int quantity = request.containsKey("quantity") ? (int) request.get("quantity") : 1;
-	
+
 		User user = userRepository.findByUsername(username)
 				.orElseThrow(() -> new IllegalArgumentException("User not found with username: " + username));
-		
+
 		cartService.addToCart(user.getUserId(), productId, quantity);
 		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
+
+	@GetMapping("items/count")
+	public ResponseEntity<Integer> getCartCount(@RequestParam String username){
+		User user=userRepository.findByUsername(username).orElseThrow(()-> new RuntimeException("User with"+username+"Not Found!!!"));
+		int count=cartService.getCartItemCount(user.getUserId());
+		return ResponseEntity.ok(count);
+	}
+
+	@GetMapping("/items")
+	public ResponseEntity<Map<String, Object>> getCartItems(HttpServletRequest request) {
+		User user = (User) request.getAttribute("authenticatedUser");
+		Map<String, Object> response = cartService.getCartItems(user.getUserId());
+		return ResponseEntity.ok(response);
+	}
+	
+	@PutMapping("/update")
+	public ResponseEntity<Void> updateCartItemQuantity(@RequestBody Map<String,Object> request){
+		String username=(String) request.get("username");
+		int productId=(int) request.get("productId");
+		int quantity=(int) request.get("quantity");
+		User user=userRepository.findByUsername(username).orElseThrow(()->new RuntimeException("User does not exist"));
+		cartService.updateCartItemQuantity(user.getUserId(), productId, quantity);
+		return ResponseEntity.status(HttpStatus.OK).build();
+	}
+	
+	@DeleteMapping("/delete")
+	public ResponseEntity<Void> deleteCartItem(@RequestBody Map<String, Object> request) {
+	    String username = (String) request.get("username");
+	    int productId = (int) request.get("productId");
+	    User user = userRepository.findByUsername(username).orElseThrow(() -> new RuntimeException("User does not exist"));
+	    cartService.deleteItem(user.getUserId(), productId);
+	    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}	
+	
 }
--- src/main/java/com/example/demo/service/CartService.java
+++ src/main/java/com/example/demo/service/CartService.java
@@ -1,5 +1,6 @@
 package com.example.demo.service;
 
+import java.util.*;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,8 +8,10 @@ import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.CartItem;
 import com.example.demo.entity.Product;
+import com.example.demo.entity.ProductImage;
 import com.example.demo.entity.User;
 import com.example.demo.repositories.CartRepository;
+import com.example.demo.repositories.ProductImageRepository;
 import com.example.demo.repositories.ProductRepository;
 import com.example.demo.repositories.UserRepository;
 
@@ -23,15 +26,18 @@ public class CartService {
 
 	@Autowired
 	ProductRepository productRepository;
+	
+	@Autowired
+	ProductImageRepository productImageRepository;
 
 	public void addToCart(int userId,int productId,int quantity) {
 
 		User user=userrepository.findById(userId)
 				.orElseThrow(() -> new IllegalArgumentException("User not found with ID: " + userId));
-		
+
 		Product product = productRepository.findById(productId)
 				.orElseThrow(() -> new IllegalArgumentException("Product not found with ID: " + productId));
-		
+
 		Optional<CartItem> existingItem = cartRepository.findByUserAndProduct(userId, productId);
 		if (existingItem.isPresent()) {
 			CartItem cartItem = existingItem.get();
@@ -41,5 +47,71 @@ public class CartService {
 			CartItem newItem = new CartItem(user, product, quantity);
 			cartRepository.save(newItem);
 		}
+	}
+
+	public int getCartItemCount(int userId) {
+		return cartRepository.getTotalCount(userId);
+	}
+
+	public Map<String,Object> getCartItems(int userId){
+		List<CartItem> cartItems=cartRepository.findCartItemsWithProductDetails(userId);
+		Map<String,Object> response=new HashMap<>();
+		User user=userrepository.findById(userId).orElseThrow(()->new RuntimeException("User Not Found"));
+		response.put("username", user.getUsername());
+		response.put("role", user.getRole().name());
+
+		List<Map<String, Object>> products = new ArrayList<>();
+		int overallTotalPrice = 0;
+
+		for (CartItem cartItem : cartItems) {
+			Map<String, Object> productDetails = new HashMap<>();
+
+			Product product = cartItem.getProduct();
+
+			List<ProductImage> productImages = productImageRepository.findByProduct_ProductId(product.getProductId());
+//			String imageUrl = (productImages != null && !productImages.isEmpty())
+//					? productImages.get(0).getImageUrl()
+//							: "default-image-url";
+			String imageUrl = productImages.get(0).getImageUrl();
+
+			productDetails.put("product_id", product.getProductId());
+			productDetails.put("image_url", imageUrl);
+			productDetails.put("name", product.getName());
+			productDetails.put("description", product.getDescription());
+			productDetails.put("price_per_unit", product.getPrice());
+			productDetails.put("quantity", cartItem.getQuantity());
+			productDetails.put("total_price", cartItem.getQuantity() * product.getPrice().doubleValue());
+
+			products.add(productDetails);
+
+			overallTotalPrice += cartItem.getQuantity() * product.getPrice().doubleValue();
+		}
+
+		Map<String, Object> cart = new HashMap<>();
+		cart.put("products", products);
+		cart.put("overall_total_price", overallTotalPrice);
+
+		response.put("cart", cart);
+
+		return response;
+	}
+	
+	public void updateCartItemQuantity(int userId,int productId,int quantity) {
+		Optional<CartItem> existingItem=cartRepository.findByUserAndProduct(userId, productId);
+		
+		if(existingItem.isPresent()) {
+			if(quantity==0) {
+				deleteItem(userId, productId);
+			}
+			else {
+				CartItem cartItem=existingItem.get();
+				cartItem.setQuantity(quantity);
+				cartRepository.save(cartItem);
+			}
+		}
+	}
+	
+	public void deleteItem(int userId,int productId) {
+		cartRepository.deleteCartItem(userId, productId);
 	}
 }
